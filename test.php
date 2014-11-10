d604cda9b94dcc98f101bff80c18f6a254d491ef
kokoi <samriche@hexaglobe.com>
2014-11-10 16:30:50 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once('ListLexer.php');
+require_once('token.php');
+//require_once('storage.php');
+
+/*
+ *  GET URL FILE
+ */
+if (empty($argv[1])){
+    die ("URL is needed\n");
+} else {
+    $url = $argv[1];
+}
+/*
+ *  GET FILE
+ */
+
+/*
+$read = fopen($url, "r") or die("unable to read file\n");
+$Data = fread($read, filesize($url));
+fclose($read);
+*/
+
+$file = file_get_contents($url);
+/*
+$lexer = new ListLexer(($file));
+$token = $lexer->nextToken();
+*/
+
+$input = 'Salut je suis un oiseau nomme sami';
+$lexer = new ListLexer($file);
+$token = $lexer->nextToken();
+
+$result = array(); // La variable qui va rassembler tous les résultats des token
+
+while($token->type != Lexer::EOF_TYPE) {
+    //echo $token . "\n";
+    $result[] = $token; // L'opérateur [] lié à un tableau permet de rajouter une "stack" à un tableau
+    $token = $lexer->nextToken();
+}
+
+$File = $lexer->getFileParsing();
+//print_r($file);
+?>
