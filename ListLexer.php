eda8baff8d584dc2da9528e6d133fb0c982d0ac5
kokoi <samriche@hexaglobe.com>
2014-11-28 16:55:37 +0100

    Update ListLexer.php

--- ListLexer.php
+++ ListLexer.php
@@ -1,12 +1,11 @@
     <?php
-
 require_once('lexer.php');
 
 class ListLexer extends Lexer {
     const NAME      = 2;
     const SHARP     = 3;
     static $tokenNames = array("n/a", "<EOF>",
-                               "NAME", "SHARP" );
+                               "NAME", "SHARP", "comment" );
     
     public function getTokenName($x) {
         return ListLexer::$tokenNames[$x];
@@ -17,9 +16,7 @@ class ListLexer extends Lexer {
     }
 
     public function getFileParsing() {
-       $token = new Token(null, null, 2);
-        //print_r ($token);
-        //pourquoi cet enculé me stock rien ?
+       $token = new Token(null, null, null, 2);
     }
 
     public function isLETTER() {
@@ -29,21 +26,20 @@ class ListLexer extends Lexer {
         $this->c <= 'Z';
     }
 
-
     public function nextToken() {
         while ( $this->c != self::EOF ) {
             switch ( $this->c ) {
                 case ' ' :  case '\t': case '\n': case '\r': $this->WS();
                            continue;
                 case '#' : /*$this->consume();*/
-                            $this->COMMENT();
+                            return $this->COMMENT();
                            //return new Token(self::SHARP, "#",1);
                 default:
                     if ($this->isLETTER() ) return $this->NAME();
                     throw new Exception("invalid character: " . $this->c);
             }
         }
-        return new Token(self::EOF_TYPE,"<EOF>",1);
+        return new Token(self::EOF_TYPE, null, "<EOF>",1);
     }
 
     /** NAME : ('a'..'z'|'A'..'Z')+; // NAME is sequence of >=1 letter */
@@ -53,18 +49,34 @@ class ListLexer extends Lexer {
             $buf .= $this->c;
             $this->consume();
         } while ($this->isLETTER());
-        
-        return new Token(self::NAME, $buf,1);
+        switch ($buf) {
+            case 'Alias' :
+                return $this->isAlias();
+            default:
+                return new Token(self::NAME, null, $buf, 1);
+        }
     }
-
-
+/** COMMENT: récupère #+ élement jusqu'à \n */
     public function COMMENT() {
         $comment = "";
-        while ($this->c != '\n' && $this->c != self::EOF){
+        while ($this->c != "\n" && $this->c != self::EOF){
             $comment .= $this->c;
             $this->consume();
         }
-        die($comment);
+            $this->consume();
+        return new Token(self::SHARP, null, $comment, 1);
+    }
+
+    public function isAlias() {
+        $option = array();
+        while ($this->c != self::EOF) {
+            while ($this->c != '\t' || $this->c != '\n' || $this->c != '\r') {
+            $options = $this->c;
+            $this->consume();
+            }
+        $this->consume();
+        }
+        return array($options);
     }
 
     /** WS : (' '|'\t'|'\n'|'\r')* ; // ignore any whitespace */
